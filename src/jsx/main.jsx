63e72a35b0b4dbd1dce71cf6c27c49b3934c059d
Filip Godziński <godzinski.filip@gmail.com>
2017-12-07 23:06:30 +0100

    clickable cookie, score info

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,11 +3,47 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', function() {
 
+  class Cookie extends React.Component {
+    render() {
+      return <button className="cookie" onClick={this.props.onClick}>Cookie</button>;
+    }
+  }
 
+  class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        score: 0
+      }
+    }
+
+    handleClick = () => {
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <Cookie onClick={this.handleClick} />
+          <h1>{this.state.score}</h1>
+        </div>
+      );
+    }
+  }
+
+  class App extends React.Component {
+    render() {
+      return (
+        <Game />
+      );
+    }
+  }
 
 
     ReactDOM.render(
-      <h1>Hello, world</h1>,
+      <App />,
       document.getElementById('app')
     );
 });
