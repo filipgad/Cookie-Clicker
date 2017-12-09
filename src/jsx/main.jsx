705dcfe76b153389f988f7f6dd48227a31b233fc
Filip Godziński <godzinski.filip@gmail.com>
2017-12-09 15:45:04 +0100

    All buttons with producers

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,44 +3,90 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', function() {
 
+  // Cookie to click
   class Cookie extends React.Component {
     render() {
       return <button className="cookie" onClick={this.props.onClick}>Cookie</button>;
     }
   }
 
-  class Cursor extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        numberOfElements: 0,
-        cookiesToActiveBtn: 5
-      }
-    }
-
-    clickCursor = () => {
-      this.setState({
-        numberOfElements: this.state.numberOfElements + 1,
-        cookiesToActiveBtn: this.state.cookiesToActiveBtn * 2
-      });
-      console.log(this.state.cookiesToActiveBtn);
-    }
-
+  // Button with producer
+  class Producer extends React.Component {
     render() {
 
       return (
-        this.props.score >= this.state.cookiesToActiveBtn ?
-          <button className="cursor storeBtn" onClick={this.clickCursor}>CURSOR! You have: {this.state.numberOfElements} cursors. For next you need: {this.state.cookiesToActiveBtn} cookies.</button>
+        this.props.score >= this.props.toActiveBtn ?
+          <button className="{this.props.name} storeBtn" onClick={this.props.clickBtn}>{this.props.name}! You have: {this.props.numberOfElements} {this.props.name}s. For next you need: {this.props.toActiveBtn} {this.props.name}s.</button>
         :
-          <button className="cursor storeBtn" disabled onClick={this.clickCursor}>CURSOR! You have: {this.state.numberOfElements} cursors. For next you need: {this.state.cookiesToActiveBtn} cookies.</button>
+          <button className="{this.props.name} storeBtn" disabled onClick={this.props.clickBtn}>{this.props.name}! You have: {this.props.numberOfElements} {this.props.name}s. For next you need: {this.props.toActiveBtn} {this.props.name}s.</button>
       );
     }
   }
 
+  // Shop with producers
   class Store extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        numberOfCursors: 0, // each numberOf... stores the number of producers we have bought
+        toActiveCursor: 5, // each toActive... stores the number of points we need to activate the producer
+        numberOfGrandmas: 0,
+        toActiveGrandma: 50,
+        numberOfFarms:0,
+        toActiveFarm: 100,
+        numberOfBakeries: 0,
+        toActiveBakery: 200,
+        numberOfMines: 0,
+        toActiveMain: 400,
+        score: this.props.score
+      }
+    }
+
+
+    clickCursor = () => {
+      this.setState({
+        numberOfCursors: this.state.numberOfCursors + 1,
+        toActiveCursor: this.state.toActiveCursor * 2
+      });
+    }
+
+    clickGrandma = () => {
+      this.setState({
+        numberOfGrandmas: this.state.numberOfGrandmas + 1,
+        toActiveGrandma: this.state.toActiveGrandma * 2
+      });
+    }
+
+    clickFarm = () => {
+      this.setState({
+        numberOfFarms: this.state.numberOfFarms + 1,
+        toActiveFarm: this.state.toActiveFarm * 2
+      });
+    }
+
+    clickBakery = () => {
+      this.setState({
+        numberOfBakeries: this.state.numberOfBakeries + 1,
+        toActiveBakery: this.state.toActiveBakery * 2
+      });
+    }
+
+    clickMain = () => {
+      this.setState({
+        numberOfMains: this.state.numberOfMains + 1,
+        toActiveMain: this.state.toActiveMain * 2
+      });
+    }
+
     render() {
       return (
-        <Cursor score={this.props.score} />
+        <div>
+          <Producer name="Cursor" score={this.props.score} clickBtn={this.clickCursor} numberOfElements={this.state.numberOfCursors} toActiveBtn={this.state.toActiveCursor}/>
+          <Producer name="Grandma" score={this.props.score} clickBtn={this.clickGrandma} numberOfElements={this.state.numberOfGrandmas} toActiveBtn={this.state.toActiveGrandma}/>
+          <Producer name="Farm" score={this.props.score} clickBtn={this.clickFarm} numberOfElements={this.state.numberOfFarms} toActiveBtn={this.state.toActiveFarm}/>
+          <Producer name="Bakery" score={this.props.score} clickBtn={this.clickBakery} numberOfElements={this.state.numberOfBakeries} toActiveBtn={this.state.toActiveBakery}/>
+          <Producer name="Main" score={this.props.score} clickBtn={this.clickMain} numberOfElements={this.state.numberOfMains} toActiveBtn={this.state.toActiveMain}/>
+        </div>
       );
     }
   }
@@ -58,8 +104,6 @@ document.addEventListener('DOMContentLoaded', function() {
         score: this.state.score + 1
       });
     }
-
-
 
     render() {
       return (
